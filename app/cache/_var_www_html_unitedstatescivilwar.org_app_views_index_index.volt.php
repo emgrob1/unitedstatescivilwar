076b5392645928aded0a4f02e27f8bb6cf3de3a7
emgrob1@yahoo.com <emgrob1@yahoo.com>
2016-07-13 05:23:40 -0400

    initial commit

--- app/cache/_var_www_html_unitedstatescivilwar.org_app_views_index_index.volt.php
+++ app/cache/_var_www_html_unitedstatescivilwar.org_app_views_index_index.volt.php
@@ -1,7 +1,31 @@
-<div class="page-header">
-    <h1>Congratulations!</h1>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<?php echo $this->partial('partials/header'); ?>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+
+  <div class="container">
+
+    <!-- Map html - add the below to your page -->
+    <div class="lg-map-wrapper" data-map="usa/usa/lg-map/usa.js">
+      <div id="lg-map"></div>
+      <div class="lg-map-text"></div>
+    </div>
+    <!-- End Map html -->
+
+  </div>
+
+
+<div id="footer">
+Copyright © 2016 Evan Grob
 </div>
 
-<p>You're now flying with Phalcon. Great things are about to happen!</p>
 
-<p>This page is located at <code>views/index/index.volt</code></p>
+</body>
+
+</html>
--- app/config/config.php
+++ app/config/config.php
@@ -7,8 +7,8 @@ return new \Phalcon\Config(array(
         'adapter'     => 'Mysql',
         'host'        => 'localhost',
         'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
+        'password'    => 'evan',
+        'dbname'      => 'unitedstatescivilwars',
         'charset'     => 'utf8',
     ),
     'application' => array(
